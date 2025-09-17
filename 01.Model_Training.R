6d9aad1bf619bfedb5af34fb59a150ad926a3311
chenliiiii <chenli5679@163.com>
2025-09-17 20:38:34 +0800

    update script

--- 01.Model_Training.R
+++ 01.Model_Training.R
@@ -1,43 +1,45 @@
 ## Model training
 
+library(e1071)
 library(caret)
 library(pROC)
 library(randomForest)
 library(ggplot2)
-library(caret)
+library(PRROC)
+library(dplyr)
+library(parallel)
 
 # load data
 df_unlabeled <- read.table("Unlabeled_dataset.txt", header = T)
 df_positive <- read.table("Positive_dataset.txt", header = T)
 
 
-# train
 set.seed(202312)
-u_fold <- createMultiFolds(y=df_unlabeled$Label, k = 10, times = 1)
 p_fold <- createMultiFolds(y=df_positive$Label, k = 10, times = 1)
 
 
-res <- list()
 rf_roc <- list()
 auc_value <- list()
+pr <- list()
 auPR <- list()
 F1score <- list()
+FNR <- list()
 rankInx <- list()
+finalPU <- list()
+res_val <- vector("list", 10)
+res_model <- list()
 
 for(i in 1:10){
   
-  train_u <- df_unlabeled[ u_fold[[i]],] 
-  test_u <- df_unlabeled[ -u_fold[[i]],] 
+  train_u <- df_unlabeled
   
   train_p <- df_positive[ p_fold[[i]],] 
-  test_p <- df_positive[-p_fold[[i]],] 
+  valid_p <- df_positive[-p_fold[[i]],] 
   
-  df_test <- rbind(test_u, test_p)
-  
-  set.seed(202313)
+  set.seed(i)
   index <- replicate(100, sample(seq_len(nrow(train_u)), replace = F, size = nrow(train_p))) 
   
-  lapply(1:100, function(x){
+  mclapply(1:100, function(x){
     print(paste0("x = ", x))
     
     train_n <- train_u[index[,x],]
@@ -50,121 +52,144 @@ for(i in 1:10){
     train_np$Label <- as.factor( train_np$Label)
     
     set.seed(202313)
-    Classifier <- randomForest(Label ~ ., data = train_np, mtry =7, ntree = 1000, importance =T)
+    Classifier <- svm(Label~.,data = train_np, kernel="radial", probability = T)
     
     
     ### Initial predicting
-    pred_rf <- predict(Classifier, newdata = train_u[,-c(1,21)], type = "prob")
-    pred_rf <- as.data.frame(pred_rf)
+    pred_svm <- predict(Classifier, newdata = train_u[,-c(1,21)], probability = T)
+    pred_svm <- attr(pred_svm, "probabilities") %>% as.data.frame()
     
-    tr_1 = 0.9
-    train_p <- rbind(train_p, train_u[ pred_rf$`1` >= tr_1, ])
+    tr_1 = 0.8
+    train_p <- rbind(train_p, train_u[ pred_svm$`1` >= tr_1, ])
     train_p$Label <- 1
     
-    tr_2 <- quantile(pred_rf$`1`, probs = seq(0,1,0.1))[3][[1]]
-    train_n <- train_u[ pred_rf$`1` <= tr_2, ]
-    train_n$Label <- 0
+    tr_2 <- 0.2
     
-    idx = c( which(pred_rf$`1` >= tr_1), which(pred_rf$`1` <= tr_2))
+    train_n <- train_u[pred_svm$`1` <= tr_2, ]
+    if (nrow(train_n) > 0) {
+      train_n$Label <- 0
+    } else {
+      tr_2 <- 0.3
+      train_n <- train_u[pred_svm$`1` <= tr_2, ]
+      train_n$Label <- 0
+    }
+    
+    idx = c( which(pred_svm$`1` >= tr_1), which(pred_svm$`1` <= tr_2))
     
     train_np <- rbind(train_p, train_n )
     train_np <- train_np[,-1]
     train_np$Label <- as.factor( train_np$Label)
     
     set.seed(202313)
-    Classifier <- randomForest(Label ~ ., data = train_np, mtry =7, ntree = 1000, importance =T)
+    Classifier <- svm(Label~.,data = train_np, kernel="radial", probability = T)
     
     
     ### again training
     train_q <- train_u[-idx, ]
     n = nrow(train_q)
-    while ( n > 0 ){
+    while ( length(idx) > 0 & n > 0 ){
+      if (nrow(train_q) == 0) break
       print(paste0("n = ", n))
       
-      pred_rf <- predict(Classifier, newdata = train_q[,-c(1,21)], type = "prob")
-      pred_rf <- as.data.frame(pred_rf)
+      pred_svm <- predict(Classifier, newdata = train_q[,-c(1,21)], probability = T)
+      pred_svm <- attr(pred_svm, "probabilities") %>% as.data.frame()
       
-      tr_1 = 0.9
-      train_p <- rbind(train_p, train_q[ pred_rf$`1` >= tr_1, ])
+      tr_1 <- 0.9
+      train_p <- rbind(train_p, train_q[ pred_svm$`1` >= tr_1, ])
       train_p$Label <- 1
       
-      tr_2 <- quantile(pred_rf$`1`, probs = seq(0,1,0.1))[3][[1]]
-      train_n <- rbind(train_n, train_q[ pred_rf$`1` <= tr_2, ])
+      tr_2 <- 0.1
+      train_n <- rbind(train_n, train_q[ pred_svm$`1` <= tr_2, ])
       train_n$Label <- 0
       
-      idx = c( which(pred_rf$`1` >= tr_1), which(pred_rf$`1` <= tr_2))
+      idx = c( which(pred_svm$`1` >= tr_1), which(pred_svm$`1` <= tr_2))
       
       train_np <- rbind(train_p, train_n )
       train_np$Label <- as.factor( train_np$Label)
       
       set.seed(202313)
-      Classifier <- randomForest(Label ~ ., data = train_np[,-1], mtry =7, ntree = 1000, importance =T)
+      Classifier <- svm(Label~.,data = train_np[,-1], kernel="radial", probability = T)
       
       train_q <- train_q[-idx, ]
       n = nrow(train_q)
       
     }
-    return( Classifier )
-  }) -> res[[i]]
+    
+    temp <- predict(Classifier, newdata = valid_p[, -c(1, 21)])
+    TPR <- sum(temp == 1)/nrow(valid_p)
+    resval <- (TPR >= 5/6) 
+    
+    res <- train_np[,c(1,21)]
+    colnames(res)[2] <- x
+    
+    return( list(val = resval, PU = res) )
+  }, mc.cores = 6) -> res_tmp
   
+  res_PU <- list()
+  for(x in 1:100) {
+    res_val[[i]][[x]] <- res_tmp[[x]]$val
+    res_PU[[x]] <- res_tmp[[x]]$PU
+  }
+  
+  ## filter replicates by TPR and calculate final PN datasets
+  finalPU[[i]] <- Reduce(function(x,y){merge(x,y, all = T)}, res_PU[unlist(res_val[[i]])])
+  finalPU[[i]] <- finalPU[[i]][ rowSums(is.na(finalPU[[i]][,-1]))/ncol(finalPU[[i]][,-1]) <= 0.2, ]
+  finalPU[[i]] <- finalPU[[i]] %>% mutate(across(-1, as.character)) %>% mutate(across(-1, as.numeric))
+  finalPU[[i]]$Mean <- rowMeans(finalPU[[i]][,-1], na.rm = T)
+  finalPU[[i]]$Label <- ifelse(finalPU[[i]]$Mean >= 0.5, 1, 0)
+  
+  
+  ## training RF
+  finalPU[[i]] <- rbind(train_p,train_u)[,-21] %>% merge(finalPU[[i]][,c("ASID", "Label")], ., by = "ASID", all.x = T)
+  finalPU[[i]]$Label <-  as.factor( finalPU[[i]]$Label)
+  finalPU[[i]] <- finalPU[[i]][,c(1, 3:21, 2)]
+  
+  set.seed(123)
+  train_index <- createDataPartition(
+    y = finalPU[[i]]$Label,     
+    p = 0.8,        
+    list = FALSE,      
+    times = 1          
+  )
+  
+  train_data <- finalPU[[i]][train_index, ]  
+  test_data  <- finalPU[[i]][-train_index, ] 
+  
+  res_model[[i]] <- randomForest(Label ~ ., data = train_data[,-1], mtry =7, ntree = 1000, importance =T)
+  resPred <- predict(res_model[[i]], newdata = test_data[,-c(1,21)], type = "prob") %>% as.data.frame()
+  
+  print("performance evaluation")
   
   ## performance evaluation
   ### roc and auc
-  lapply(1:100, function(y) {
-    temp <- predict(res[[i]][[y]], newdata = df_test[, -c(1, 21)], type = "prob") %>% as.data.frame()
-    temp[,2]
-  }) %>% do.call(cbind, .)  -> tmp0
-  
-  obs_p_rf <- data.frame(pred = rowMeans(tmp0),
-                         obs = df_test$Label)
-  
+  obs_p_rf <- data.frame(pred = resPred[,2], obs = test_data$Label)
   rf_roc[[i]] <- roc(obs ~ pred, obs_p_rf, levels = c("0", "1" ))
   auc_value[[i]] <- as.numeric(auc(rf_roc[[i]]))
-  
   
   ### auPR
   fg <- obs_p_rf[obs_p_rf$obs == 1, "pred"]
   bg <- obs_p_rf[obs_p_rf$obs == 0, "pred"]
-  pr <- pr.curve(scores.class0 = fg, scores.class1 = bg, curve = T)
-  
-  auPR[[i]] <- pr$auc.integral
-  
+  pr[[i]] <- pr.curve(scores.class0 = fg, scores.class1 = bg, curve = T)
+  auPR[[i]] <- pr[[i]]$auc.integral
   
   ### rank index
   obs_p_rf <- obs_p_rf[order(obs_p_rf$pred, decreasing = T), ]
   obs_p_rf$Rank <- 1:nrow(obs_p_rf)
   SP <- subset(obs_p_rf, obs == 1)
-  
   rankInx[[i]] <- (1/nrow(SP)) * (sum(SP$Rank)/nrow(obs_p_rf))
   
-  
-  ### F1 score
-  lapply(1:100, function(y) {
-    
-    temp <- predict(res[[i]][[y]], newdata = test_p[,-c(1,21)])
-    temp <- as.character(temp) %>% as.numeric()
-    
-  }) %>% do.call(cbind, .) %>% rowMeans(.) -> tmp0
-  
-  
-  tmp <- confusionMatrix((ifelse(tmp0 >= 0.5, "1", "0") %>% as.factor()), 
-                         factor(rep("1",length(tmp0)), levels = c(0,1)),
+  ### F1 score and false negative ratio
+  tmp <- confusionMatrix((ifelse(obs_p_rf$pred >= 0.5, "1", "0") %>% as.factor()),
+                         obs_p_rf$obs,
                          mode = "everything",
                          positive = "1")
-  
   F1score[[i]] <- tmp$byClass["F1"]
+  recall <- tmp$byClass["Recall"]
+  FNR[[i]] <- 1 - as.numeric(recall)
+  
 }
 
-which.max(unlist(auc_value))
-which.max(unlist(auPR))
-which.max(unlist(F1score))
-which.min(unlist(rankInx))
-
-finalModel <- res[[which.max(unlist(auc_value))]]
-
-save(auc_value, auPR, F1score, rankInx, file = "Model_evaluation.RData")
-saveRDS(finalModel, file = "finalModel.Rds")
-
-
+save(res_val, finalPU, rf_roc, auc_value, pr, auPR, F1score, FNR, rankInx, file = "Model_evaluation.RData")
+saveRDS(res_model, file = "finalModel.Rds")
 
 
--- 02.Prediction.R
+++ 02.Prediction.R
@@ -3,12 +3,8 @@
 library(FAScore)
 library(dplyr)
 
-## HD
-SPECIES <- c("HomSap", "MusMus")
-LINEAGE <- c("Erythroid", "Lymphoid", "Myeloid")
 
-meta <- readRDS("metainfo.Rds")
-my_gtf <- list(HomSap = "Homo_sapiens.GRCh38.101.gtf",
+mygtf <- list(HomSap = "Homo_sapiens.GRCh38.101.gtf",
                MusMus = "Mus_musculus.GRCm38.101.gtf",
                MacMul = "Macaca_mulatta.Mmul_10.101.gtf",
                RatNor = "Rattus_norvegicus.Rnor_6.0.101.gtf",
@@ -16,9 +12,18 @@ my_gtf <- list(HomSap = "Homo_sapiens.GRCh38.101.gtf",
 model = readRDS("finalModel.Rds")
 
 
+
 ### calculate dynamic score
+#### HD
+SPECIES <- c("HomSap", "MusMus")
+LINEAGE <- c("Erythroid", "Lymphoid", "Myeloid")
+
+meta <- readRDS("metainfo.Rds")
+
+
 FAScore_HD <- list()
-for( s in SPECIES[2]){
+for( s in SPECIES[1:2]){
+  df_gtf <- rtracklayer::readGFF(mygtf[[s]])
   for(l in LINEAGE){
     df_AS <- read.table(paste0(s,"_",l,"_AS.txt"), header = T)
     colnames(df_AS) <- gsub("[.]","-",colnames(df_AS))
@@ -28,18 +33,20 @@ for( s in SPECIES[2]){
     colnames(df_Gene) <- gsub("[.]","-",colnames(df_Gene))
     
     df_AS <- df_AS[apply(df_AS, 1, function(x) {sum(x >= 0.05 & x <= 0.95, na.rm = T) >= 2}), ]
+    df_Gene_pc <- df_Gene[unique(df_gtf$gene_id), ]
+    df_Iso_pc <- subset(df_Iso, gene_id %in% unique(df_gtf$gene_id))
     
     MyObj <-FAScoreDataSet(colData = meta[[s]][[l]], 
                            AS = df_AS, 
-                           GENE = df_Gene,
-                           ISOFORM = df_Iso)
+                           GENE = df_Gene_pc,
+                           ISOFORM = df_Iso_pc)
     
     MyObj <- CalcuFeature(MyObj, group.by = "CellType", cores = 10)
-    MyObj <- CalcuDyScore(MyObj, maxRange = 10, maxSlope = 1, type = "Gene")
-    MyObj <- CalcuDyScore(MyObj, maxRange = 1, maxSlope = 1, type = "AS")
-    MyObj <- ASmapIso(MyObj, gtf = my_gtf[[s]], AStype = "exonic", cores = 10)
+    MyObj <- CalcuDyScore(MyObj, maxSlope = 1, type = "Gene")
+    MyObj <- CalcuDyScore(MyObj, maxSlope = 1, type = "AS")
+    MyObj <- ASmapIso(MyObj, gtf = mygtf$BM[[s]], AStype = "exonic", cores = 10)
     MyObj <- ChooseIso(MyObj)
-    MyObj <- matchAppris(MyObj, gtf = my_gtf[[s]], species = s)
+    MyObj <- matchAppris(MyObj, gtf = mygtf$BM[[s]], species = s)
     MyObj <- CalcuFAScore(MyObj, model = model)
     MyObj <- calcuGMM(MyObj, LOG = 2)
     FAScore_HD[[s]][[l]] <- MyObj
@@ -53,46 +60,35 @@ for( s in SPECIES[2]){
 Species <- c("HomSap", "MacMul", "MusMus", "RatNor", "DanRer")
 FHOmeta <- readRDS("FL.meta.Rds")
 
-
-
 FAScore_FHO <- list()
 for( s in Species){
   df_AS <- read.table(paste0(,s,"_FHO_AS.txt"), header = T)
-  colnames(df_AS) <- gsub("[.]","-",colnames(df_AS))
   df_Gene <- read.table(paste0(,s,"_FHO_Gene.txt"), header = T)
-  colnames(df_Gene) <- gsub("[.]","-",colnames(df_Gene))
   df_Iso <- read.table(paste0(,s,"_FHO_Iso.txt"), header = T)
-  colnames(df_Gene) <- gsub("[.]","-",colnames(df_Gene))
   
   df_AS <- df_AS[apply(df_AS, 1, function(x) {sum(x >= 0.05 & x <= 0.95, na.rm = T) >= 2}), ]
+  df_gtf <- rtracklayer::readGFF(mygtf$FL[[s]])
+  df_Gene_pc <- df_Gene[unique(df_gtf$gene_id), ]
+  df_Iso_pc <- subset(df_Iso, gene_id %in% unique(df_gtf$gene_id))
   
-  MyObj <-FAScoreDataSet(colData = FHOmeta[[s]], 
-                         AS = df_AS, 
-                         GENE = df_Gene,
-                         ISOFORM = df_Iso)
+  MyObj <-FAScoreDataSet(colData = FHOmeta[[s]],
+                         AS = df_AS,
+                         GENE = df_Gene_pc,
+                         ISOFORM = df_Iso_pc)
   
   MyObj <- CalcuFeature(MyObj, group.by = "StageAbbreviation", cores = 10)
-  MyObj <- CalcuDyScore(MyObj, maxRange = 10, maxSlope = 1, type = "Gene")
-  MyObj <- CalcuDyScore(MyObj, maxRange = 1, maxSlope = 1, type = "AS")
-  MyObj <- ASmapIso(MyObj, gtf = my_gtf[[s]], AStype = "exonic", cores = 10)
+  MyObj <- CalcuDyScore(MyObj, maxSlope = 1, type = "Gene")
+  MyObj <- CalcuDyScore(MyObj, maxSlope = 1, type = "AS")
+  MyObj <- ASmapIso(MyObj, gtf = mygtf$FL[[s]], AStype = "exonic", cores = 10)
   MyObj <- ChooseIso(MyObj)
-  MyObj <- matchAppris(MyObj, gtf = my_gtf[[s]], species = s)
+  MyObj <- matchAppris(MyObj, gtf = mygtf$FL[[s]], species = s)
   MyObj <- CalcuFAScore(MyObj, model = model)
   MyObj <- calcuGMM(MyObj, LOG = 2)
   FAScore_FHO[[s]] <- MyObj
 }
 
 
-
-
-
 saveRDS(FAScore_HD, file = "FAScore_HD.Rds")
 saveRDS(FAScore_FHO, file = "FAScore_FHO.Rds")
-
-
-
-
-
-
 
 
